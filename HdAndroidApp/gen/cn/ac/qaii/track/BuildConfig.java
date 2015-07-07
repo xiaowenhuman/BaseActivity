5eb79209cca81d4f83bdab96c1988a4f737445d6
xiaowenhuman <zhongxiaowenhuman@163.com>
2015-07-07 10:13:13 +0800

    first commit

---
+++ HdAndroidApp/gen/cn/ac/qaii/track/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cn.ac.qaii.track;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ HdAndroidApp/src/cn/ac/qaii/track/activity/MainActivity.java
@@ -0,0 +1,14 @@
+package cn.ac.qaii.track.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import cn.ac.qaii.track.R;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
